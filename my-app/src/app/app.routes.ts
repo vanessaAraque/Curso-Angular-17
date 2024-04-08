2565859c18a5214a87147ca78d16887d58dbe5ef
vanessaAraque <130965740+vanessaAraque@users.noreply.github.com>
2024-04-08 10:00:51 +0000

    Cambio a loadComponent, sin cargar todas páginas

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -5,7 +5,10 @@ import { Pagina3Component } from './pages/pagina3/pagina3.component';
 
 export const routes: Routes = [
 
-    {path:"pagina1",component:Pagina1Component},
-    {path:"pagina2",component:Pagina2Component},
-    {path:"pagina3",component:Pagina3Component}
+    {path:"pagina1",
+        loadComponent:() => import('./pages/pagina1/pagina1.component').then(c=>c.Pagina1Component)},
+    {path:"pagina2",
+        loadComponent:() => import('./pages/pagina2/pagina2.component').then(c=>c.Pagina2Component)},
+    {path:"pagina3",
+        loadComponent:() => import('./pages/pagina3/pagina3.component').then(c=>c.Pagina3Component)}
 ];
