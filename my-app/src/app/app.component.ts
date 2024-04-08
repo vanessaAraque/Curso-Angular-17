71543fb9ee9c3e24dfbeda0726c2c97d312c1248
vanessaAraque <130965740+vanessaAraque@users.noreply.github.com>
2024-04-08 08:38:13 +0000

    Creación del timer

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar/calendar.component';
 import { FormComponent } from './controls/form/form.component';
+import { CounterComponent } from './counter/counter.component';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports:[CalendarComponent,AppComponent,FormComponent],
+  imports:[CalendarComponent,AppComponent,FormComponent,CounterComponent, TimerComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
